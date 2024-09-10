3ee1216a4fec9c94e09322565234a710df3a502f
Nick Piaddo <nick.piaddo@protonmail.com>
2024-09-10 06:20:14 +0000

    Create module `core::macros`

--- src/core/macros/mod.rs
+++ src/core/macros/mod.rs
@@ -1,15 +1,8 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Core objects and helper functions.
-
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod errors;
-pub mod iter;
-pub(crate) mod macros;
-pub mod partition;
-pub mod prompt;
