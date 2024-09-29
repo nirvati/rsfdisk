e92b8bae879fab7bdb3a1bcbb0a8ffbcf5fdfd44
Nick Piaddo <nick.piaddo@protonmail.com>
2024-09-29 18:00:18 +0000

    Create module `core::private`

--- src/core/private/mod.rs
+++ src/core/private/mod.rs
@@ -1,16 +1,8 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Core objects and helper functions.
-
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod errors;
-pub mod iter;
-pub(crate) mod macros;
-pub mod partition;
-pub(crate) mod private;
-pub mod prompt;
